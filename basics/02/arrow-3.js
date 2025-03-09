89f1b2c934e6d168523736bf03655a6d400155e9
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2025-03-09 16:22:15 +0900

    자바스크립트 기본문법1

--- basics/02/arrow-3.js
+++ basics/02/arrow-3.js
@@ -1,9 +1,11 @@
 // 매개변수가 있는 함수 표현식을 화살표 함수로 표현하기  (결과 비교 : 02\results\arrow-3.js)
 
 // 함수 표현식
-// let sum = function (a, b) {
+// let sum = function (a, b) {  //여기의 ab는 파라미터이구
 //   return a + b;
 // }
-// console.log(sum(100, 200));
+// console.log(sum(100, 200));  // 여기의 100,200은 argument 즉, 인수이다
 
 // 화살표 함수
+let sum = (a, b) => a + b;
+console.log(sum(444, 453));
--- basics/02/function-1.js
+++ basics/02/function-1.js
@@ -1,7 +1,7 @@
 // 함수 선언하고 호출하기  (결과 비교 파일 : 02\results\function-1.js)
-
 // 함수 선언
-
-
+function greeting(name) {
+    console.log(`${name}님, 안녕하소`);
+}
 // 함수 호출
-
+greeting('민중');
